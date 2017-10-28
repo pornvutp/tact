42bb15633c12c5e4ef8ebf15b4938e367850fa81
pornvutp <pornvutp@gmail.com>
2017-10-28 13:13:35 +0700

    First Test

---
+++ command
@@ -0,0 +1,4 @@
+cd Documents/GitHub/tact
+npm install
+npm start
+npm install bootstrap@4.0.0-beta.2
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
+import NavBar from './components/navbar';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
+  <App />
   , document.querySelector('.container'));
